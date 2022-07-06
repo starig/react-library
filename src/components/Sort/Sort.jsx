8cc52746dfa579d45fcbb5ce1d79d1a5546b5c75
srarig <starkovivan3@gmail.com>
2022-07-06 14:08:06 +0800

    Fix relevance/newest

--- src/components/Sort/Sort.jsx
+++ src/components/Sort/Sort.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styles from './Sort.module.scss';
 import {useDispatch, useSelector} from "react-redux";
 import {setSortValue} from "../../redux/slices/searchSlice";
 import {fetchBooks} from "../../redux/slices/itemsSlice";
-import loader from './../../assets/loader.gif';
 import {useLocation, useParams} from "react-router-dom";
 
 const Sort = () => {
@@ -14,22 +13,32 @@ const Sort = () => {
     const { pathname } = useLocation();
     const params = useParams();
 
+    useEffect(() => {
+        console.log('changed: ', sortValue);
+        dispatch(fetchBooks({inputValue, page, sortValue}));
+    }, [sortValue]);
+
     const setNewest = () => {
         dispatch(setSortValue('newest'));
-        dispatch(fetchBooks({inputValue, page, sortValue}));
+        console.log(sortValue);
     }
 
     const setRelevance = () => {
         dispatch(setSortValue('relevance'));
-        dispatch(fetchBooks({inputValue, page, sortValue}));
     }
 
     return (
         <div className={styles.sort}>
-            <button disabled={items.length === 0 || pathname === `/book/${params.id}`} className={`${styles.sortItem} ${sortValue === 'relevance' && styles.active}`} onClick={setRelevance}>
+            <button disabled={items.length === 0 || pathname === `/book/${params.id}`}
+                    className={`${styles.sortItem} ${sortValue === 'relevance' && styles.active}`} onClick={() => {
+                setRelevance()
+            }}>
                 Relevance
             </button>
-            <button disabled={items.length === 0 || pathname === `/book/${params.id}`} className={`${styles.sortItem} ${sortValue === 'newest' && styles.active}`} onClick={setNewest}>
+            <button disabled={items.length === 0 || pathname === `/book/${params.id}`}
+                    className={`${styles.sortItem} ${sortValue === 'newest' && styles.active}`} onClick={() => {
+                setNewest()
+            }}>
                 Newest
             </button>
 
